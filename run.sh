ddc450d6a50c07a604685a12affda6d5df8f8cf8
Paul Dumoulin <paul.l.dumoulin@gmail.com>
2019-07-18 00:28:57 +0000

    switches working in python3
    get IR working and add comments
    
    move files over to replace old ones

--- run.sh
+++ run.sh
@@ -5,9 +5,10 @@ export PYTHONPATH=$PYTHONPATH:/home/pi/ir-tools
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 uwsgi \
-    --plugin /usr/lib/uwsgi/plugins/python_plugin.so \
+    --plugin python3 \
     --http-socket :1994 \
     --logto $DIR/logs/uwsgi.log \
     --pidfile $DIR/var/app.pid \
     --wsgi-file $DIR/web.py \
     --touch-reload $DIR/web.py
+
